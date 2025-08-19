8375453c2d60ec525601644c1d0069701da5ad72
SneakeAce <sergey_acebox@mail.ru>
2025-08-19 10:31:36 +0300

    Initial commit

---
+++ Assets/CupHeroes/Scripts/Entities/Character/Character.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Collider2D _collider;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    private PlayerInput _playerInput;
+
+
+}
